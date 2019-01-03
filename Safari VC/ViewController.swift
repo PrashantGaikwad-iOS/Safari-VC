bfba27fe64fca2af78afd88fbed9f4e3b48a9928
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-03 22:16:15 +0530

    Opening Safari VC using SafariServices

--- Safari VC/ViewController.swift
+++ Safari VC/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    // MARK: - IBActions
+    
+    @IBAction func openLinkInSafari(_ sender: Any) {
+        showSafariVC(for: "https://www.youtube.com/watch?v=gnjXbR2eNDE")
+    }
+    
+    func showSafariVC(for url: String) {
+        guard let url = URL(string: url) else {
+            // Show an invalid URL error
+            return
+        }
+        
+        let safariVC = SFSafariViewController(url: url)
+        present(safariVC, animated: true)
+    }
 
 }
 
